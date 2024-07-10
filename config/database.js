d3df4d74d69c3a16548852deada68b18fe4d858f
Olubunmi Ogunjemiyo <bjemiyor@gmail.com>
2024-07-10 14:57:49 +0100

    added online sequelize

--- config/database.js
+++ config/database.js
@@ -28,17 +28,21 @@ module.exports = pool
 */
 
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.POSTGRES_URL,
-        dialect: "postgres",
-        port: process.env.DB_PORT,
-        logging: false,
-    }
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USERNAME,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.POSTGRES_URL,
+//         dialect: "postgres",
+//         port: process.env.DB_PORT,
+//         logging: false,
+//     }
 
-)
+// )
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
+    dialect: 'postgres'
+   })
 
 module.exports = sequelize
