b7697a6a7c319bfe33ec0c597912d95675563460
Olubunmi Ogunjemiyo <bjemiyor@gmail.com>
2024-07-11 15:46:04 +0100

    removed express-validator check

--- route/authRoute.js
+++ route/authRoute.js
@@ -3,7 +3,8 @@ const { validateRegistration, validateLogin } = require('../validator');
 
 const router = require('express').Router();
 
-router.route('/register').post(validateRegistration,signup)
+// router.route('/register').post(validateRegistration,signup)
+router.route('/register').post(signup)
 router.route('/login').post( validateLogin, login)
 
 module.exports = router;
